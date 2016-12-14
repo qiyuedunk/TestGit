6fa84c8d17af647f3da0c8d2f8f240606f981e18
qiyue <qiyue@firevale.com>
2016-12-14 12:23:19 +0800

    no message

---
+++ hello2.lua
@@ -0,0 +1,17 @@
+function hello()
+  print("hello")
+end
+
+function sayHello()
+  print("a new function")
+end
+
+
+function sayHelloTest()
+  print("this is local b")
+end
+
+
+function cc()
+  print("this is local b")
+end
