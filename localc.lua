3132d05b6d7b222e45ceaaef1955da04f149682c
qiyue <qiyue@firevale.com>
2016-12-14 12:25:33 +0800

    no message

---
+++ localc.lua
@@ -0,0 +1,17 @@
+function hello()
+  print("hello")
+end
+
+function sayHello()
+  print("a new function")
+end
+
+
+function sayHelloTest()
+  print("this is local b")
+end
+
+
+function localc()
+  print("this is local b")
+end
