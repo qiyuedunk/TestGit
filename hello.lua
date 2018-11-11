3e4804b7c5107f5c9db6bc8ea022024e91bf4463
qiyue <756860442@qq.com>
2018-11-11 12:28:12 +0800

    merge squash feature

--- hello.lua
+++ hello.lua
@@ -14,6 +14,8 @@ end
 function feature()
 	print("=====feature 1=====")
 	print("=====feature 2=====")
-	print("=====feature 3=====")	
+	print("=====feature 3=====")
+	print("=====feature 6=====")
+	print("=====feature 7=====")	
 end
 
